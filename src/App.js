130ed3d062a1c04153a6eff50d5f9e49e403c3f1
msaucier <msaucier@commandalkon.com>
2021-11-04 09:09:00 -0500

    online-offline demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
+import useOnlineStatus from './useOnlineStatus';
+import { useEffect, useState, useRef } from 'react';
+import moment from 'moment';
 
 function App() {
+  const {onlineStatus} = useOnlineStatus();
+  const [statusArray, setStatusArray] = useState([]);
+
+  useEffect(() => {
+    setStatusArray(previousArray => {
+      let newArray = [...previousArray];
+      newArray.unshift({
+        onlineStatus,
+        statusTime: moment().toISOString(),
+        count: (newArray[0]?.count || 0) + 1,
+      })
+  
+      return newArray.slice(0, Math.min(newArray.length, 100));
+    })
+  }, [onlineStatus])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <table>
+          <thead>
+            <th>Count</th>
+            <th>Time</th>
+            <th>Status</th>
+          </thead>
+          <tbody>
+            {statusArray.map((n) => {
+              return <tr key={n.statusTime}>
+                <td>{n.count}</td>
+                <td>{n.statusTime}</td>
+                <td>{n.onlineStatus ? 'ONLINE' : 'OFFLINE'}</td>
+              </tr>
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
